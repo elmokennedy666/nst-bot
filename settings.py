c7bead8cb25d49c54ec1a5a8679a76ed159f5d08
Elmo <rus755835@gmal.com>
2020-06-28 13:52:22 +0500

    'init'

---
+++ settings.py
@@ -0,0 +1,2 @@
+TOKEN = '1393457415:AAGHThyEYg1-uLpRXsgN31koxDAI62d6vXM'
+URL = 'https://nst-bot.herokuapp.com/'
--- telbot.py
+++ telbot.py
@@ -6,15 +6,15 @@ from telebot import types
 import time
 from transforming_functions import run_style_transfer, cnn_normalization_std, cnn_normalization_mean, unload, \
     image_loader
-
+from settings import TOKEN, URL
 
 def start():
-    bot = telebot.TeleBot('1393457415:AAGHThyEYg1-uLpRXsgN31koxDAI62d6vXM', threaded=False)
+    bot = telebot.TeleBot(TOKEN, threaded=False)
     server = Flask(__name__)
     id = {}
     cnn = torch.load('m.pth')
 
-    @server.route('/' + '1393457415:AAGHThyEYg1-uLpRXsgN31koxDAI62d6vXM', methods=['POST'])
+    @server.route('/' + TOKEN, methods=['POST'])
     def get_message():
         bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
         return "!", 200
@@ -22,7 +22,7 @@ def start():
     @server.route("/")
     def webhook():
         bot.remove_webhook()
-        bot.set_webhook(url='https://nst-bot.herokuapp.com/' + '1393457415:AAGHThyEYg1-uLpRXsgN31koxDAI62d6vXM')
+        bot.set_webhook(url = URL + TOKEN)
         return "!", 200
 
     @bot.message_handler(commands=['start'])
@@ -70,24 +70,6 @@ def start():
     I will send you transformed image, when it wll be ready!
     '''
 
-    '''
-    @bot.message_handler(content_types=['photo'])
-    def get_img(message):
-        style_file_id, content_file_id = message.photo[-2].file_id, message.photo[-1].file_id
-        bot.send_message(message.chat.id, bot_transfer_text)
-        save_image(message.chat.id, style_file_id, '_style.jpg')
-        save_image(message.chat.id, content_file_id, '_content.jpg')
-        style_img = image_loader(str(message.chat.id) + '_style.jpg')
-        content_img = image_loader(str(message.chat.id) + '_content.jpg')
-        input_img = image_loader(str(message.chat.id) + '_content.jpg')
-        cnn = torch.load('m.pth')
-        transformed_image = run_style_transfer(cnn, cnn_normalization_mean, cnn_normalization_std, style_img, content_img, input_img, 200)
-        import time
-        time.sleep(100)
-        unload(transformed_image).save(str(message.chat.id) + '_transformed.jpg')
-        with open(str(message.chat.id) + '_transformed.jpg', 'rb') as file_read:
-            bot.send_photo(message.chat.id, file_read)
-    '''
 
     @bot.message_handler(commands=['transform'])
     def transform(message):
